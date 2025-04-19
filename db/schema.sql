18620d6c1b6c2b306e4b20ca90f61010d606114a
jsensada <jordisensadatorres@gmail.com>
2025-04-19 12:05:53 +0200

    Initializing db with some sample data

--- db/schema.sql
+++ db/schema.sql
@@ -5,5 +5,11 @@ CREATE TABLE IF NOT EXISTS users (
     date_of_birth DATE NOT NULL
 );
 
+-- Insert sample data
+INSERT INTO users (username, date_of_birth) VALUES
+('foo', '1994-11-18'),
+('bar', '2025-04-12');
+
+
 -- Grant access to the table
 GRANT ALL ON TABLE users TO birthday;
