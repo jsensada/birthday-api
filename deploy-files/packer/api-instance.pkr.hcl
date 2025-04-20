3fdb470872b2408d4ac5d37fa5755d98307c551d
jsensada <jordisensadatorres@gmail.com>
2025-04-20 14:00:57 +0200

    Install gunicorn an manage image versioning

--- deploy-files/packer/api-instance.pkr.hcl
+++ deploy-files/packer/api-instance.pkr.hcl
@@ -22,7 +22,7 @@ source "googlecompute" "ubuntu_image" {
   zone                = var.zone
   ssh_username        = var.ssh_username
   machine_type        = "e2-medium"
-  image_name          = "birthday-api"
+  image_name          = "birthday-api-{{timestamp}}"
 }
 
 build {
--- deploy-files/packer/files/customize.sh
+++ deploy-files/packer/files/customize.sh
@@ -2,14 +2,14 @@
 
 ## Installations content:
 sudo DEBIAN_FRONTEND=noninteractive apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get install python3 python3-pip -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get install python3 python3-pip gunicorn -y
 
 ## Setting up the birthday-api app
 sudo mkdir /opt/birthday-api
 sudo mv /tmp/app.py /opt/birthday-api/app.py
 sudo adduser birthday-api --gecos "" --disabled-password
+sudo pip3 install -r /tmp/requirements.txt --break-system-packages
 sudo chown -R birthday-api:birthday-api /opt/birthday-api
-sudo pip install -r /tmp/requirements.txt
 
 ## Create a systemd service for birthday-api
 sudo mv /tmp/birthday-api.service /etc/systemd/system/birthday-api.service
