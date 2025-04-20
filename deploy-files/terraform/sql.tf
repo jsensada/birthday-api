1c41e36f6f26f5db37c657efe4667041630ed626
jsensada <jordisensadatorres@gmail.com>
2025-04-20 11:01:28 +0200

    Moving terraform files into deploy-files

--- deploy-files/terraform/sql.tf
+++ deploy-files/terraform/sql.tf
@@ -1,15 +1,16 @@
 resource "google_sql_database_instance" "brithday_db" {
-  name = "birthday-db"
+  name                = "birthday-db"
   deletion_protection = false
-  database_version = "MYSQL_8_4"
-  region = "europe-southwest1"
+  database_version    = "MYSQL_8_4"
+  region              = "europe-southwest1"
   settings {
-    tier = "db-f1-micro"
+    tier    = "db-e2-small"
+    edition = "ENTERPRISE"
   }
 }
 
 resource "google_sql_user" "birthday_user" {
-  name = data.google_secret_manager_secret_version.birthday_db_username.secret_data
+  name     = data.google_secret_manager_secret_version.birthday_db_username.secret_data
   password = data.google_secret_manager_secret_version.birthday_db_password.secret_data
   instance = google_sql_database_instance.brithday_db.name
 }
