b819d038cf52ab54d988c63ae8c57245e853db75
jsensada <jordisensadatorres@gmail.com>
2025-04-21 00:39:21 +0200

    Fix - typo: Wrong secrets name

--- deploy-files/terraform/secrets.tf
+++ deploy-files/terraform/secrets.tf
@@ -1,11 +1,11 @@
 data "google_secret_manager_secret_version" "birthday_db_username" {
   provider = google
-  secret   = "brithday-db-username"
+  secret   = "birthday-db-username"
   project  = var.project_id
 }
 
 data "google_secret_manager_secret_version" "birthday_db_password" {
   provider = google
-  secret   = "brithday-db-password"
+  secret   = "birthday-db-password"
   project  = var.project_id
 }
