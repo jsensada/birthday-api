8f35a6b6134fef355d6c8a7f7554ec4bdbbf8b38
jsensada <jordisensadatorres@gmail.com>
2025-04-20 20:20:03 +0200

    creating the basic schema from terraform execution

--- deploy-files/terraform/sql.tf
+++ deploy-files/terraform/sql.tf
@@ -1,4 +1,4 @@
-resource "google_sql_database_instance" "brithday_db" {
+resource "google_sql_database_instance" "birthday_db" {
   name                = "birthday-db"
   deletion_protection = false
   database_version    = "MYSQL_8_4"
@@ -11,7 +11,7 @@ resource "google_sql_database_instance" "brithday_db" {
 }
 
 resource "google_sql_database" "birthday_db_schema" {
-  name     = "birthdays"
+  name     = "birthday"
   instance = google_sql_database_instance.birthday_db.name
   charset  = "utf8mb4"
 }
@@ -19,5 +19,19 @@ resource "google_sql_database" "birthday_db_schema" {
 resource "google_sql_user" "birthday_user" {
   name     = data.google_secret_manager_secret_version.birthday_db_username.secret_data
   password = data.google_secret_manager_secret_version.birthday_db_password.secret_data
-  instance = google_sql_database_instance.brithday_db.name
+  instance = google_sql_database_instance.birthday_db.name
+}
+
+resource "null_resource" "init_db_schema" {
+  depends_on = [google_sql_database.birthday_db_schema]
+
+  provisioner "local-exec" {
+    command = <<EOT
+      mysql -h ${google_sql_database_instance.birthday_db.public_ip_address} \
+            -u ${data.google_secret_manager_secret_version.birthday_db_username.secret_data} \
+            -p${data.google_secret_manager_secret_version.birthday_db_password.secret_data} \
+            -D birthdays \
+            -e "CREATE TABLE IF NOT EXISTS users (username VARCHAR(50) PRIMARY KEY,date_of_birth DATE NOT NULL);"
+    EOT
+  }
 }
